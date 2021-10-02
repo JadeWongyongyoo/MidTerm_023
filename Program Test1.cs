33bd982dbdb0022c27f3fccb762ae13686c915c3
JadeWongyoungyoo <nawaf.w1@gmail.com>
2021-10-03 01:56:06 +0700

    No Test 2

---
+++ Program Test1.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Test_HangMan
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Headder();
+            string[] listwords = new string[3];
+            listwords[0] = "Tennis";
+            listwords[1] = "footbll";
+            listwords[2] = "badminton";
+            Random randGen = new Random();
+            var resultRandom = randGen.Next(0,3);
+            string Word = listwords[resultRandom];
+        }
+
+        // สร้าง
+        static void Headder()
+        {
+            Console.WriteLine("Welcome to Hangman Game");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("1. Play game");
+            Console.WriteLine("2. Exit");
+            Console.Write("Please Select Menu: ");
+            int number = int.Parse(Console.ReadLine());
+            while (number > 3)
+            {
+                Console.ReadLine();
+                number = int.Parse(Console.ReadLine());
+            }
+            if (number == 1)
+            {
+                Startgame();
+            }
+            else if (number == 2)
+            {
+
+            }
+
+        }
+        static void Startgame()
+        {
+            Console.WriteLine("Play Game Hangman");
+            Console.WriteLine("----------------------------------------");
+            Console.WriteLine("Incorrect Score: 0");
+            Console.WriteLine("---------------------");
+            Console.WriteLine("Input letter alphabet: ");
+            Console.ReadLine();
+            string Input = Console.ReadLine();
+        }
+        
+    }
+}
---
+++ Program Test3.cs
@@ -0,0 +1,103 @@
+﻿using System;
+using System.Collections.Generic;
+namespace Test_3
+{
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ChoosMenu();
+            ListFlower();
+            Choice();
+        }
+        static void ChoosMenu()
+        {
+            Console.Clear();
+            string decide = "y";
+            string selectFlower;
+            FlowerStore flowerStore = new FlowerStore();
+            do
+            {
+                Console.WriteLine("Select number for buy flower : ");
+                Console.WriteLine("----------------------------------------------------");
+                foreach (string i in flowerStore.flowerList)
+                {
+                    Console.Write((flowerStore.flowerList.IndexOf(i) + 1) + " ");
+                    Console.WriteLine(i);
+                }
+                selectFlower = Console.ReadLine();
+
+                
+
+                Console.WriteLine("You can stop this progress ? exit for >> exit << progress and press any key for continue");
+
+                decide = Console.ReadLine();
+                if (decide == "exit")
+                {
+                    Console.Write("Current my cart");
+                    flowerStore.showCart();
+                }
+            }
+            while (decide != "exit");
+
+        }
+
+        static FlowerStore flowerStore;
+        static void ListFlower ()
+        {
+            Program.flowerStore = new FlowerStore();
+        }
+
+        static void Choice(string selectFlower)
+        {
+            switch (selectFlower)
+            {
+                case "1":
+                    flowerStore.addToCart(flowerStore.flowerList[0]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[0]);
+                    break;
+                case "2":
+                    flowerStore.addToCart(flowerStore.flowerList[1]);
+                    Console.WriteLine("Added " + flowerStore.flowerList[1]);
+                    break;
+                default:
+                    Console.WriteLine("Not Added to cart. found select number of flower");
+                    break;
+            }
+        }
+    }
+        
+    class FlowerStore
+    {
+            public List<string> flowerList = new List<string>();
+        List<string> cart = new List<string>();
+        public FlowerStore()
+        {
+            flowerList.Add("Rose");
+            flowerList.Add("Lotus");
+        }
+        public void addToCart(string name)
+        {
+            cart.Add(name);
+        }
+
+        public void showCart()
+        {
+            if (cart.Count == 0)
+            {
+                Console.WriteLine("Cart is empty");
+                Console.WriteLine("----------------------------------------------------");
+            }
+            else
+            {
+                Console.WriteLine("My Cart :");
+                foreach (string i in cart)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+        } 
+    }
+
+}
